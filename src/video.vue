b77b756a50684417755d5b778c11e9f8351686c5
no9 <anton@venshare.com>
2017-08-29 14:46:18 +0100

    new blue map

---
+++ src/video.vue
@@ -0,0 +1,7 @@
+<template>
+  <video width="500px" height="350px" autobuffer="autobuffer" autoplay="autoplay" loop="loop" controls="controls">
+    <source src='video/VID-20170519-WA0024.mp4' type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+  </video>
+  <a href="contact.html">Get In Touch</a>
+</template>
+
